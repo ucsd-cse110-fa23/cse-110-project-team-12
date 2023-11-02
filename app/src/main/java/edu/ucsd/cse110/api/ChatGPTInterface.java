4acfd63c4ad57afc59f01d7ad72fe56607ffc87c
Shawn Clayton <114379755+skclayton@users.noreply.github.com>
2023-11-01 18:30:51 -0700

    Developed ChatGPT.java, still have a "JSONObject["choices"] not found" error

--- app/src/main/java/edu/ucsd/cse110/api/ChatGPTInterface.java
+++ app/src/main/java/edu/ucsd/cse110/api/ChatGPTInterface.java
@@ -1,6 +1,10 @@
 package edu.ucsd.cse110.api;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 // Simple interface for interchanging between real and mock GPT during testing
 public interface ChatGPTInterface {
-	public String[] promptGPT(String mealType, String ingredients);
+	public String[] promptGPT(String mealType, String ingredients) 
+			throws IOException, InterruptedException, URISyntaxException;
 }
