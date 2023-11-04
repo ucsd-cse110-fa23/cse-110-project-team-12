babf3c77cf576b51f964034a0e426fbc4b0fe5a9
Akshat30 <akshat30@gmail.com>
2023-11-03 17:25:23 -0700

    Fixed bug for ChatGPT, added code to main method to compare Mock and Real ChatGPT files

--- app/src/main/java/edu/ucsd/cse110/api/ChatGPTMock.java
+++ app/src/main/java/edu/ucsd/cse110/api/ChatGPTMock.java
@@ -1,24 +1,36 @@
 package edu.ucsd.cse110.api;
 
-// Mock ChatGPT to use for testing, onstruced with integer to set the model response
+// Mock ChatGPT to use for testing, construced with integer to set the model response
 public class MockChatGPT implements ChatGPTInterface {
 	private int desiredResponse;
 
-	// Constructor for given variables
+	/**
+	 * default constructor
+	 */
+	public MockChatGPT() {
+		this.desiredResponse = 1;
+	}
+
+	/**
+	 * 
+	 * @param desiredResponse which given response you want
+	 */
 	public MockChatGPT(int desiredResponse) {
 		this.desiredResponse = desiredResponse;
 	}
 
 	/**
-     * Fake prompt to ChatGPT
+	 * Fake prompt to ChatGPT
 	 * 
-	 * @param mealType mealType is not used but needs to be passed in to match the interface
-	 * @param ingredients ingredients is not used but needs to be passed in to match the interface
+	 * @param mealType    mealType is not used but needs to be passed in to match
+	 *                    the interface
+	 * @param ingredients ingredients is not used but needs to be passed in to match
+	 *                    the interface
 	 * 
-     * @return String[] array whose first entry is the generated name and 
-	 * 				    whose second entry is the rest of the generated text
-     */
-    public String[] promptGPT(String mealType, String ingredients) {
+	 * @return String[] array whose first entry is the generated name and
+	 *         whose second entry is the rest of the generated text
+	 */
+	public String[] promptGPT(String mealType, String ingredients) {
 		String name = "";
 		String information = "";
 
@@ -29,10 +41,12 @@ public class MockChatGPT implements ChatGPTInterface {
 					"For the Taco Bowls:\n" + //
 					"\n" + //
 					"4 large avocados, ripe but still firm\n" + //
-					"1 pound ground beef (or ground turkey, chicken, or a meat alternative for a vegetarian version)\n" + //
+					"1 pound ground beef (or ground turkey, chicken, or a meat alternative for a vegetarian version)\n"
+					+ //
 					"1 small onion, finely chopped\n" + //
 					"2 cloves garlic, minced\n" + //
-					"1 packet of taco seasoning mix or your own blend of taco spices (chili powder, cumin, paprika, oregano, salt, and pepper)\n" + //
+					"1 packet of taco seasoning mix or your own blend of taco spices (chili powder, cumin, paprika, oregano, salt, and pepper)\n"
+					+ //
 					"1 cup cooked rice (white or brown)\n" + //
 					"1 cup black beans, drained and rinsed\n" + //
 					"1 cup corn kernels (fresh, frozen, or canned)\n" + //
@@ -45,11 +59,15 @@ public class MockChatGPT implements ChatGPTInterface {
 					"Instructions:\n" + //
 					"\n" + //
 					"Prepare the Avocado Bowls:\n" + //
-					"Cut the avocados in half and remove the pits. Carefully scoop out some of the flesh from each half to create a small well, making sure not to pierce through the avocado skin. This will create space for your taco fillings.\n" + //
+					"Cut the avocados in half and remove the pits. Carefully scoop out some of the flesh from each half to create a small well, making sure not to pierce through the avocado skin. This will create space for your taco fillings.\n"
+					+ //
 					"Cook the Meat:\n" + //
-					"In a skillet, heat some oil over medium heat. Add the chopped onion and garlic and sauté until they are soft and fragrant.\n" + //
-					"Add the ground meat and cook until it's browned and cooked through. Break it up into small pieces as it cooks.\n" + //
-					"Add the taco seasoning and follow the package instructions or use your own blend of spices. Stir well and let it simmer for a few minutes. If using a seasoning packet, add the recommended amount of water.\n" + //
+					"In a skillet, heat some oil over medium heat. Add the chopped onion and garlic and sauté until they are soft and fragrant.\n"
+					+ //
+					"Add the ground meat and cook until it's browned and cooked through. Break it up into small pieces as it cooks.\n"
+					+ //
+					"Add the taco seasoning and follow the package instructions or use your own blend of spices. Stir well and let it simmer for a few minutes. If using a seasoning packet, add the recommended amount of water.\n"
+					+ //
 					"Prepare the Fillings:\n" + //
 					"Cook the rice according to package instructions and set it aside.\n" + //
 					"Warm the black beans and corn in a separate pot or microwave, then set them aside.\n" + //
@@ -80,16 +98,32 @@ public class MockChatGPT implements ChatGPTInterface {
 					"Grill the chicken breasts until they are fully cooked, then dice them into bite-sized pieces.\n" + //
 					"Cook the bacon until it's crispy, then crumble it into small bits.\n" + //
 					"Hard-boil the eggs, cool them, and chop them into small pieces.\n" + //
-					"Halve the cherry tomatoes, chop the avocado, crumble the blue cheese, and finely chop the red onion.\n" + //
+					"Halve the cherry tomatoes, chop the avocado, crumble the blue cheese, and finely chop the red onion.\n"
+					+ //
 					"Assemble the Salad:\n" + //
-					"In a large salad bowl or on individual serving plates, arrange a layer of chopped romaine lettuce.\n" + //
+					"In a large salad bowl or on individual serving plates, arrange a layer of chopped romaine lettuce.\n"
+					+ //
 					"Arrange the Toppings:\n" + //
-					"In rows or sections on top of the lettuce, arrange the diced chicken, hard-boiled egg, cherry tomatoes, avocado, blue cheese, crumbled bacon, and chopped red onion.\n" + //
+					"In rows or sections on top of the lettuce, arrange the diced chicken, hard-boiled egg, cherry tomatoes, avocado, blue cheese, crumbled bacon, and chopped red onion.\n"
+					+ //
 					"Serve:\n" + //
 					"Serve your Chopped Cobb Salad with your favorite salad dressing on the side, allowing everyone to drizzle dressing over their salad as desired.";
 		}
 
-		String[] result = {name, information};
+		String[] result = { name, information };
 		return result;
+	}
+
+	public static void main(String[] args) {
+		ChatGPTInterface mock = new MockChatGPT(1);
+		ChatGPTInterface real = new ChatGPT();
+		try {
+			System.out
+					.println("\n \n Mock GPT Test: \n \n" + mock.promptGPT("lunch", "I have chicken, lemon, pepper.")[1]);
+			System.out
+					.println("\n \n Real GPT Test: \n \n" + real.promptGPT("lunch", "I have chicken, lemon, pepper.")[1]);
+		} catch (Exception e) {
+			System.err.println("Error: " + e.getMessage());
+		}
 	}
 }
