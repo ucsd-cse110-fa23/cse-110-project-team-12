780ab0789f13c52090b54d31681c76c8a3beb060
Eric Huang <41460388+praxxus11@users.noreply.github.com>
2023-11-05 00:14:27 -0700

    added mocks, tests, and refined voiceprompt

--- app/src/main/java/edu/ucsd/cse110/api/VoicePrompt.java
+++ app/src/main/java/edu/ucsd/cse110/api/VoicePrompt.java
@@ -3,37 +3,35 @@ package edu.ucsd.cse110.api;
 import java.io.File;
 import javax.sound.sampled.*;
 
-public class VoicePrompt {
-    private static AudioFormat audioFormat;
-    private static TargetDataLine targetDataLine;
-    private static Thread t;
-    private static AudioFormat getAudioFormat() {
-        // the number of samples of audio per second.
-        // 44100 represents the typical sample rate for CD-quality audio.
-        float sampleRate = 44100;
+public class VoicePrompt implements VoicePromptInterface {
+    private AudioFormat audioFormat;
+    private TargetDataLine targetDataLine;
+    private Thread t;
+    private String saveFile;
 
+    public VoicePrompt(String sf) {
+        // the number of samples of audio per second.
+        float sampleRate = 44100;
         // the number of bits in each sample of a sound that has been digitized.
         int sampleSizeInBits = 16;
-
         // the number of audio channels in this format (1 for mono, 2 for stereo).
         int channels = 1;
-
         // whether the data is signed or unsigned.
         boolean signed = true;
-
         // whether the audio data is stored in big-endian or little-endian order.
         boolean bigEndian = false;
 
-        return new AudioFormat(
-                sampleRate,
-                sampleSizeInBits,
-                channels,
-                signed,
-                bigEndian);
+        audioFormat = new AudioFormat(
+            sampleRate,
+            sampleSizeInBits,
+            channels,
+            signed,
+            bigEndian);
+        
+        saveFile = sf;
     }
-    
-    public static void startRecording() {
-        audioFormat = getAudioFormat();
+
+    public void startRecording() {
         try {
             // the format of the TargetDataLine
             DataLine.Info dataLineInfo = new DataLine.Info(
@@ -46,16 +44,16 @@ public class VoicePrompt {
             targetDataLine.open(audioFormat);
             targetDataLine.start();
             
-            t = new Thread(new Runnable(){
+            t = new Thread(new Runnable() {
                 @Override
-                public void run(){
+                public void run() {
                     try{
                         // the AudioInputStream that will be used to write the audio data to a file
                         AudioInputStream audioInputStream = new AudioInputStream(
                                 targetDataLine);
 
                         // the file that will contain the audio data
-                        File audioFile = new File("local_storage/recording.wav");
+                        File audioFile = new File(saveFile);
                         AudioSystem.write(
                                     audioInputStream,
                                     AudioFileFormat.Type.WAVE,
@@ -71,10 +69,10 @@ public class VoicePrompt {
         }
 	}
 
-	public static File stopRecording() {
+	public File stopRecording() {
         targetDataLine.stop();
         targetDataLine.flush();
         targetDataLine.close();
-		return new File("local_storage/recording.wav");
+		return new File(saveFile);
 	}
 }
