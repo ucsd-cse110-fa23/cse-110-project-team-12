132f98c6e559ee1749782ecf28b5c6de2579894e
Akshat30 <akshat30@gmail.com>
2023-12-05 00:05:15 -0800

    fixing last test to check width/height instead

--- app/src/test/java/edu/ucsd/cse110/server/services/UtilsTest.java
+++ app/src/test/java/edu/ucsd/cse110/server/services/UtilsTest.java
@@ -124,16 +124,15 @@ public class UtilsTest {
 
     @Test
     public void testDecodeBase64ToBufferedImage() {
-        int width = 2;
-        int height = 2;
-        BufferedImage expectedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        int expectedWidth = 2;
+        int expectedHeight = 2;
 
         String encodedImage = "/9j/4AAQSkZJRgABAgAAAQABAAD/2wBDAAgGBgcGBQgHBwcJCQgKDBQNDAsLDBkSEw8UHRofHh0aHBwgJC4nICIsIxwcKDcpLDAxNDQ0Hyc5PTgyPC4zNDL/2wBDAQkJCQwLDBgNDRgyIRwhMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjL/wAARCAACAAIDASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwD5/ooooA//2Q==";
 
         BufferedImage actualImage = Utils.decodeBase64ToBufferedImage(encodedImage);
 
         assertNotNull(actualImage);
-        assertEquals(width, actualImage.getWidth());
-        assertEquals(height, actualImage.getHeight());
+        assertEquals(expectedWidth, actualImage.getWidth());
+        assertEquals(expectedHeight, actualImage.getHeight());
     }
 }
